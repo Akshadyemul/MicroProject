b48db4984e47784c9abf3568ea3bf8a4e57f7d09
Akshad Yemul <akshadyemul05@gmail.com>
2024-03-18 17:17:20 +0530

    micro project completed

--- app/src/main/java/com/akshadyemul/microproject/MainActivity.java
+++ app/src/main/java/com/akshadyemul/microproject/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +19,16 @@ public class MainActivity extends AppCompatActivity {
         String lname = fromLogin.getStringExtra("username");
 
         TextView name = findViewById(R.id.name);
+        Button btnLogout = findViewById(R.id.btnLogout);
 
         name.setText("Welcome Back \n"+lname);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
